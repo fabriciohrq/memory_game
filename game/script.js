f0fe7acc92bc5f3ebe4425bd986e3d3fc02dcef7
Fabricio Henrique <fabricio.fh086@gmail.com>
2024-03-22 11:09:08 -0300

    feat: lock board and shuffling cards (finish)

--- game/script.js
+++ game/script.js
@@ -1,10 +1,14 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let travaBoard = false;
 let primeiroCard, segundoCard;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(travaBoard) return;
+    if(this === primeiroCard) return;
+
+    this.classList.add('flip');
     // console.log("Click!");
     // console.log(this);
 
@@ -12,15 +16,68 @@ function flipCard(){
         // primeiro click
         hasFlippedCard = true;
         primeiroCard = this;
-    } else{
+
+        return;
+    }
+    
         // segundo click
-        hasFlippedCard = false;
         segundoCard = this;
 
-        // match
-        console.log(primeiroCard.dataset.framework);
-        console.log(segundoCard.dataset.framework);
-    }
+        validaMatch();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+// condition ? exp1 (true) : exp2 (false)
+
+function validaMatch(){
+    let deuMatch = primeiroCard.dataset.framework === segundoCard.dataset.framework;
+    // match
+    console.log(primeiroCard.dataset.framework);
+    console.log(segundoCard.dataset.framework);
+
+    deuMatch ? congelaCards() : reiniciaCards();
+
+    // if(deuMatch){
+    //     // deu match
+    //     congelaCards();
+    // } else{
+    //     // não deu match
+    //     reiniciaCards();
+    // }
+
+    console.log('Função executada');
+}
+
+function congelaCards(){
+    primeiroCard.removeEventListener('click', flipCard);
+    segundoCard.removeEventListener('click', flipCard);
+
+    reiniciaBoard();
+}
+
+function reiniciaCards(){
+    travaBoard = true;
+
+    setTimeout(() => {
+        primeiroCard.classList.remove('flip');
+        segundoCard.classList.remove('flip');  
+
+        reiniciaBoard();
+    }, 1000);
+}
+
+function reiniciaBoard(){
+    [hasFlippedCard, travaBoard] = [false, false];
+    [primeiroCard, segundoCard] = [null, null];
+}
+
+// IIFE (Immediately Invoked FUNCTION Expression) 
+(function embaralhar(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        // Math.floor = retorna o menor número INTEIRO do parâmetro
+        // Math.random =  retornar um número pseudoaleatório de ponto flutuante entre 0 e 1.
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
